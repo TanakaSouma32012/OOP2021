2f8431d790a0f7b4fba90b398c074be5f5c7670f
田中 颯真 <32012@ojs.ac.jp>
2021-05-25 10:21:21 +0900

    p52ﾘｽﾄ2-6 変換処理をクラスとして分離

--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -6,41 +6,33 @@ using System.Threading.Tasks;
 
 namespace DistanceConverter {
     class Program {
+        //static FeetConverter converter = new FeetConverter();
         static void Main(string[] args) {
             if (args.Length >= 1 && args[0] == "-tom") {
                 //フィートからメートルへの対応表を出力
-                PrintFeetToMeterList(1,15);
-
+                PrintFeetToMeterList(1, 15);
             } else {
                 //メートルからフィートへの対応表を出力
-                PrintMeterToFeetList(1,20);
+                PrintMeterToFeetList(1, 20);
             }
-
         }
         //メートルからフィートへの対応表を出力
-        private static void PrintMeterToFeetList(int start , int stop) {
+        private static void PrintMeterToFeetList(int start, int stop) {
+            FeetConverter converter = new FeetConverter();
             for (int meter = start; meter <= stop; meter++) {
-                double feet = MeterToFeet(meter);
+                double feet = converter.FromMeter(meter);
                 Console.WriteLine("{0} m = {1:0.0000}ft", meter, feet);
             }
         }
+
         //フィートからメートルへの対応表を出力
         private static void PrintFeetToMeterList(int start, int stop) {
+            FeetConverter converter = new FeetConverter();
             for (int feet = start; feet <= stop; feet++) {
-                double meter = FeetToMeter(feet);
+                double meter = converter.ToMeter(feet);
                 Console.WriteLine("{0} ft = {1:0.0000}m", feet, meter);
             }
         }
-
-        //フィートからメートルを求める
-        static double FeetToMeter(int feet) {
-            return feet * 0.3048;
-        }
-        //メートルからフィートを求める
-        static double MeterToFeet(int meter) {
-            return meter / 0.3048;
-        }
-
-       
+        
     }
 }

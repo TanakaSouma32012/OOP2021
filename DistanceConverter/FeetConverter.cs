314e4ca4532855c7296191703f5883ebfd94b76d
田中 颯真 <32012@ojs.ac.jp>
2021-05-25 11:06:33 +0900

    p55ﾘｽﾄ2.10 定数を定義（0.3048）

--- DistanceConverter/FeetConverter.cs
+++ DistanceConverter/FeetConverter.cs
@@ -8,13 +8,15 @@ namespace DistanceConverter {
     //フィートとメートルの単位変換クラス
     public static class FeetConverter {
         //メートルからフィートを求める
+        private const double ratio = 0.3048;
+
         public static　double FromMeter(double meter) {
-            return meter / 0.3048;
+            return meter / ratio;
         }
 
         //フィートからメートルを求める
         public static double ToMeter(double feet) {
-            return feet * 0.3048;
+            return feet * ratio;
         }
     }
 }

a671f2722e4b8a70546bbb79f6af1cd12e462552
田中 颯真 <32012@ojs.ac.jp>
2021-06-01 11:24:37 +0900

    p79 2.3 完成

---
+++ Exercise3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3 {
+    class Program {
+        static void Main(string[] args) {
+            var sales = new SalesCounter("Sales.csv");
+            
+            var amountParCategory = sales.GetPerCategorySales(); //店舗別売り上げを求める
+            foreach (var obj in amountParCategory) {
+                Console.WriteLine("{0} {1:#,#}円", obj.Key, obj.Value);
+            }
+        }
+    }
+}

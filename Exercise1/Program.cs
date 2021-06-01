ed034ebde3f393d96aa218657490fa16fe02f28c
田中 颯真 <32012@ojs.ac.jp>
2021-06-01 09:34:58 +0900

    p79 2.1 完成

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 namespace Exercise1 {
     class Program {
         static void Main(string[] args) {
+            var songs0 = new Song[] {
+                new Song("","",0)
+            };
+            PrintSongs(songs0);
+        }
+        private static void PrintSongs(Song[] songs) {
+            foreach (var song in songs) {
+                Console.WriteLine("{0},{1},{2:m\\:ss}",song.Title,song.ArtistName,TimeSpan.FromSeconds( song.Length));
+            }
         }
     }
 }
--- Exercise1/Song.cs
+++ Exercise1/Song.cs
@@ -6,9 +6,15 @@ using System.Threading.Tasks;
 
 namespace Exercise1 {
     class Song {
+        public string Title { get; set; }     //曲のタイトル
+        public string ArtistName { get; set; }  //アーティストの名前
+        public int Length { get; set; }         //演奏時間、単位は秒
 
-        string Title;
-        string ArtistName;
-        int Length;
+        public Song(string title, string artistNmae, int length) {
+            Title = Title;
+            ArtistName = artistNmae;
+            Length = length;
+        }
     }
 }
+

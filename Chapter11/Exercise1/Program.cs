6b0383297ff5333bbb6384418891b9c382900f21
颯真 田中 <32012@ojs.ac.jp>
2021-08-27 15:34:05 +0900

    問題11_1_3途中

---
+++ Chapter11/Exercise1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = "Sample.xml";
+            Exercise1_1(file);
+            Console.WriteLine("-------------------");
+
+            Exercise1_2(file);
+            Console.WriteLine("-------------------");
+
+            Exercise1_3(file);
+            Console.WriteLine("-------------------");
+            
+        }
+
+        private static void Exercise1_1(string file)
+        {
+            var xdoc = XDocument.Load(file); //XMLファイルのロード
+            var novelists = xdoc.Root.Elements();
+            foreach (var novelist in novelists)
+            {
+                var xname = novelist.Element("name");
+                var xteammembers = novelist.Element("teammembers");
+                Console.WriteLine("{0} {1}",xname.Value,xteammembers.Value);
+            }
+        }
+
+        private static void Exercise1_2(string file)
+        {
+            var xdoc = XDocument.Load(file); //XMLファイルのロード
+            var novelists = xdoc.Root.Elements().OrderBy(x => (int)x.Element("firstplayed"));
+            foreach (var novelist in novelists)
+            {
+                var name = novelist.Element("name");
+                var kanzi = name.Attribute("kanji");
+                var firstplayed = novelist.Element("firstplayed");
+                Console.WriteLine("{0} {1}", kanzi.Value,firstplayed.Value);
+            }
+
+        }
+
+        private static void Exercise1_3(string file)
+        {
+            var xdoc = XDocument.Load(file); //XMLファイルのロード
+            var novelists = xdoc.Root.Elements().Max(x => x.Element("teammembers"));
+            
+        }
+
+       
+
+       
+    }
+}
--- Chapter11/Section01/Program.cs
+++ Chapter11/Section01/Program.cs
@@ -11,17 +11,44 @@ namespace Section01
     {
         static void Main(string[] args)
         {
-            var xdoc = XDocument.Load("novelists.xml");
+            var novelists = ReadNovelists();
 
-            foreach (var xnovelist in xdoc.Root.Elements())
+            foreach (var novelist in novelists)
             {
-                var xname = xnovelist.Element("name");
-                var works = xnovelist.Element("masterpieces")
-                                     .Elements("title")
-                                     .Select(x => x.Value) ;
-               
-                Console.WriteLine("{0} {1}",xname.Value, string.Join(",",works));
+                Console.WriteLine("{0} ({1}-{2}) - {3}",
+                                   novelist.Name , novelist.Birth.Year , novelist.Death.Year
+                                   ,string .Join(",",novelist.Masterpieces));
             }
+        }
+        //カスタムクラスのオブジェクトとして要素を取り出す
+        static public IEnumerable<Novelist> ReadNovelists()
+        {
+            var xdoc = XDocument.Load("novelists.xml"); //XMLファイルのロード
+            var novelists = xdoc.Root.Elements()
+                                   .Select(X => new Novelist
+                                   {
+                                       Name = (string)X.Element("name"),
+                                       KanaName　= (string)(X.Element("name").Attribute("kana")),
+                                       Birth = (DateTime)X.Element("birth"),
+                                       Death = (DateTime)X.Element("death"),
+                                       Masterpieces = X.Element("masterpieces")
+                                                        .Elements("title")
+                                                        .Select(title => title.Value)
+                                                        .ToArray()
+                                   });
+            return novelists.ToArray();
         }
     }
 }
+
+
+
+
+//var xdoc = XDocument.Load("novelists.xml");
+//var xtitles = xdoc.Root.Elements()
+//                       .Select(X => new
+//                       {
+//                           Name = (string)X.Element("name"),
+//                           Birth = (DateTime)X.Element("birth"),
+//                           Death = (DateTime)X.Element("death")
+//                       });

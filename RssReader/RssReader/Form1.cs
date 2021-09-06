07014bcf3ffb3fc8f181fc1dc573efb0667307af
颯真 田中 <32012@ojs.ac.jp>
2021-09-06 15:28:54 +0900

    タイトル表示完成

---
+++ RssReader/RssReader/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader
+{
+    public partial class Form1 : Form
+    {
+        private object cityCode;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btRead_Click(object sender, EventArgs e)
+        {
+            setRssTitle(tbUrl.Text);
+
+        }
+        private void setRssTitle(string uri)
+        {
+            using (var wc = new WebClient())
+            {
+                wc.Headers.Add("Content-type", "charset=UTF-8");
+                var uriString = string.Format(
+                   @"{0}", uri, cityCode);
+                var url = new Uri(uriString);
+                var stream = wc.OpenRead(url);
+
+                XDocument xdoc = XDocument.Load(stream);
+                var nodes = xdoc.Root.Descendants("title");
+                foreach (var node in nodes)
+                {
+                    var u = Regex.Replace(node.Value, "【|】", "");
+                    lbTitles.Items.Add(u);
+                }
+            }
+        }
+    }
+}
--- WeatherApp/WeatherApp/Form1.Designer.cs
+++ WeatherApp/WeatherApp/Form1.Designer.cs
@@ -60,7 +60,6 @@ namespace WeatherApp
             this.button1.TabIndex = 2;
             this.button1.Text = "天気表示";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
